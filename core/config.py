fe1cf5ffff58e4ff4112c43bfa33d4f83ea508c0
Felipe Adeildo <contato@felipeadeildo.com>
2025-04-10 15:48:43 -0300

    refactor: rename configuration variables for clarity and consistency

--- core/config.py
+++ core/config.py
@@ -2,17 +2,19 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    host: str = "lara-bomb.insper.dev"
-    port: int | None = 8000
-    secret_key: str = "vrau-easter-egg"
-    debug: bool = False
-    access_token_expire_minutes: int = 60 * 24 * 7
-    algorithm: str = "HS256"
+    server_bind: str = "0.0.0.0"
+    server_port: int | None = 8000
+    server_secret_key: str = "vrau-easter-egg"
+    server_debug: bool = False
+    server_access_token_expire_minutes: int = 60 * 24 * 7
+    server_algorithm: str = "HS256"
 
-    title: str = "Lara Bomb"
-    width: int = 800
-    height: int = 600
-    fps: int = 60
+    server_endpoint: str = "lara-bomb.insper.dev"
+
+    client_title: str = "Lara Bomb"
+    client_width: int = 800
+    client_height: int = 600
+    client_fps: int = 60
 
     class Config:
         env_file = ".env"
